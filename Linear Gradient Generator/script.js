83d6f05607478c47abdcc369a19b8b52b765b3a0
[mdmohsin] <[roni07wd@gmail.com]>
2023-09-18 13:46:10 +0300

    javascript added

---
+++ Linear Gradient Generator/script.js
@@ -0,0 +1,21 @@
+const form = document.querySelector('form')
+const color1 = document.querySelector("#color1")
+const color2 = document.querySelector("#color2")
+const directions = document.querySelectorAll('input[name="direction"]')
+const answer = document.querySelector('.code')
+
+const bgGenerator = () => {
+let selectedValue;
+
+    for (const item of directions) {
+        if (item.checked){
+    selectedValue = item.value
+}
+    }
+    let gradient = `linear-gradient(${selectedValue}, ${color1.value}, ${color2.value})`
+    document.body.style.backgroundImage = gradient
+    answer.textContent = gradient
+}
+
+
+form.addEventListener('change', bgGenerator)
